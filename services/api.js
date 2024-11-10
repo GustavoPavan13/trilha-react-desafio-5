4a4ea99ec50e4bcff1d6ee833717bf8da0ba2bd2
Gustavo Pavan <gustavo.pavan@hotmail.com>
2024-11-10 00:08:19 -0300

    resolução trilha react 5

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
+//biblioteca utilizada para fazer requisições HTTP  
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    //url base da api
+    baseURL: 'https://okdpdhkcohsfwyegfshz.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+         apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZHBkaGtjb2hzZnd5ZWdmc2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMDY3ODEsImV4cCI6MjA0Njc4Mjc4MX0.ayLNjaW4vDSxuwlNuZXnh6PaLhRb58U-2h6VFquEXJ0",
+         authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZHBkaGtjb2hzZnd5ZWdmc2h6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMDY3ODEsImV4cCI6MjA0Njc4Mjc4MX0.ayLNjaW4vDSxuwlNuZXnh6PaLhRb58U-2h6VFquEXJ0",
     }
 })
